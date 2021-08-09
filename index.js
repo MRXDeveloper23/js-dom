a766fdc6320a0b0545eed1d279e93fce73af0e16
Khurmatillo <72973599+MRXDeveloper23@users.noreply.github.com>
2021-08-09 16:28:17 +0500

    task3

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const money = prompt("Pul miqdorini kiriting(UZSda): ");
-const ticket = 500 * 9433.34;
-const hotelFee = 250 * 9433.34;
-const trip = 120 * 10354.03;
-const sum = ticket + hotelFee + trip;
-
-if (money >= sum) {
-  console.log("Oq yo’l, Alisher!");
-} else {
-  console.log("Alisher, ozgina sabr qilish kerak bo’lar ekan.");
-}
+const date = new Date();
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const day = date.getDay();
+const currentTime = date.toLocaleTimeString();
+alert(`Today is: ${weekDays[day]}\nCurrent time is: ${currentTime}`);
